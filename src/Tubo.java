2cab12905bc63f47698a09f126045cb5fa76b941
Alex <alex.baratella@buonarroti.tn.it>
2023-09-26 18:29:55 +0200

    feat: creazione metodo per il calcolo della superficie

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,3 +1,12 @@
 public class Tubo {
+    private float superficie, volume;
 
+    public Tubo(float raggio){
+        this.superficie = calcoloSuperficie(raggio);
+        this.volume = calcoloVolume(raggio);
+    }
+
+    public float calcoloSuperficie(float raggio){
+
+    }
 }
