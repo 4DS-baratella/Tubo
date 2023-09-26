ceed44ff898daa2cb73be879dc97390fad77acc7
Alex <alex.baratella@buonarroti.tn.it>
2023-09-26 18:37:46 +0200

    fix: aggiunta variabile altezza

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,16 +1,16 @@
 public class Tubo {
     private double superficie, volume;
 
-    public Tubo(float raggio){
+    public Tubo(float raggio, float altezza){
         this.superficie = calcoloSuperficie(raggio);
-        this.volume = calcoloVolume(raggio);
+        this.volume = calcoloVolume(raggio, altezza);
     }
 
     public double calcoloSuperficie(float raggio){
         return 2 * (3.14) * raggio;
     }
 
-    public double calcoloVolume(float raggio){
+    public double calcoloVolume(float raggio, float altezza){
 
     }
 }
