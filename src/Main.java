00e3fe6810ec588d810337656fe97c12ac52b780
Alex <alex.baratella@buonarroti.tn.it>
2023-09-26 18:56:29 +0200

    feat: creazione menu

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int scelta = -1;
+        do{
+            System.out.println("\n0. Per uscire dal programma;" +
+                    "\n1. Per visualizzare la superificie del tubo;" +
+                    "\n2. Per visualizzare il volume del tubo;" +
+                    "\n\nInserire: ");
+
+        }while(scelta != 0);
     }
 }
